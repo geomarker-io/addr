cfc836ff55da30d1c3d7255ccea549a16ec77e52
Cole Brokamp <cole.brokamp@gmail.com>
2025-09-18 13:57:32 -0400

    remove cagis_addr() function in favor of codec::cincy_addr_geo()

--- R/data.R
+++ R/data.R
@@ -17,26 +17,6 @@ nad_addr <- function() {
   readRDS(fs::path_package("addr", "NAD_OH_Hamilton.rds"))
 }
 
-#' CAGIS Addresses (Q4 2024)
-#'
-#' @returns An example tibble created from the CAGIS addresses with a pre-calculated, unique `cagis_addr` vector column.
-#' The `cagis_addr_data` column is a list of tibbles because one CAGIS address can correspond to multiple
-#' parcel identifiers and address-level data (place, type, s2, etc.).
-#' See `inst/make_cagis_addr.R` for source code to create data, including filtering criteria:
-#'
-#' - use only addresses that have `STATUS` of `ASSIGNED` or `USING` and are not orphaned (`ORPHANFLG == "N"`)
-#' - omit addresses with `ADDRTYPE`s that are milemarkers (`MM`), parks (`PAR`), infrastructure projects (`PRJ`),
-#'   cell towers (`CTW`), vacant or commercial lots (`LOT`), and other miscellaneous non-residential addresses (`MIS`, `RR`, `TBA`)
-#' - s2 cell is derived from LONGITUDE and LATITUDE fields in CAGIS address database
-#'
-#' This export is not the latest version of CAGIS address data, but that can be installed with `codec::cincy_addr_geo()`
-#' @export
-#' @examples
-#' cagis_addr()
-cagis_addr <- function() {
-  readRDS(fs::path_package("addr", "cagis_addr.rds"))
-}
-
 #' Example real-world addresses
 #'
 #' The voter_addresses data was generated as an example character vector of real-world addresses.
