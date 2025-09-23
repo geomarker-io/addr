5d80adcdf40fa0fa383391344ba1277ab51e71c5
Cole Brokamp <cole.brokamp@gmail.com>
2025-09-23 14:29:28 -0400

    fix text now that "#" is not removed

--- tests/testthat/test-tag.R
+++ tests/testthat/test-tag.R
@@ -6,21 +6,44 @@ test_that("addr_tag works", {
     "111 State Route 32 Cincinnati OH 45912"
   )) |>
     expect_equal(
-      list(c(
-        AddressNumber = "290", StreetName = "Ludlow", StreetNamePostType = "Avenue",
-        OccupancyType = "Apt", OccupancyIdentifier = "2", PlaceName = "Cincinnati",
-        StateName = "OH", ZipCode = "45220"
-      ), c(
-        AddressNumber = "200",
-        StreetNamePreDirectional = "W", StreetName = "14th", StreetNamePostType = "Street",
-        PlaceName = "Cincinnati", StateName = "OH", ZipCode = "45222"
-      ), c(
-        AddressNumber = "3333", StreetName = "Burnet", StreetNamePostType = "Ave",
-        PlaceName = "Cincinnati", StateName = "OH", ZipCode = "45219"
-      ), c(
-        AddressNumber = "111", StreetNamePreType = "State", StreetNamePreType = "Route",
-        StreetName = "32", PlaceName = "Cincinnati", StateName = "OH",
-        ZipCode = "45912"
-      ))
+      list(
+        c(
+          AddressNumber = "290",
+          StreetName = "Ludlow",
+          StreetNamePostType = "Avenue",
+          OccupancyType = "Apt",
+          OccupancyIdentifier = "#",
+          OccupancyIdentifier = "2",
+          PlaceName = "Cincinnati",
+          StateName = "OH",
+          ZipCode = "45220"
+        ),
+        c(
+          AddressNumber = "200",
+          StreetNamePreDirectional = "W",
+          StreetName = "14th",
+          StreetNamePostType = "Street",
+          PlaceName = "Cincinnati",
+          StateName = "OH",
+          ZipCode = "45222"
+        ),
+        c(
+          AddressNumber = "3333",
+          StreetName = "Burnet",
+          StreetNamePostType = "Ave",
+          PlaceName = "Cincinnati",
+          StateName = "OH",
+          ZipCode = "45219"
+        ),
+        c(
+          AddressNumber = "111",
+          StreetNamePreType = "State",
+          StreetNamePreType = "Route",
+          StreetName = "32",
+          PlaceName = "Cincinnati",
+          StateName = "OH",
+          ZipCode = "45912"
+        )
+      )
     )
 })
