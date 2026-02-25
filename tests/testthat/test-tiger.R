696bdbbf85e5417c071c4ed8f3fcf05a00130129
Cole Brokamp <cole.brokamp@gmail.com>
2026-02-25 09:15:52 -0500

    more clear tests for tiger

--- tests/testthat/test-tiger.R
+++ tests/testthat/test-tiger.R
@@ -1,25 +1,29 @@
 test_that("can download files from tiger", {
   skip_if_offline()
   skip_on_cran()
-  ## # works
-  ## curl -L "https://www2.census.gov/geo/tiger/TIGER2022/ADDRFEAT/tl_2022_39061_addrfeat.zip" \
-  ##   -H "Host: www2.census.gov" \
-  ##   -H "User-Agent: addr package for R (https://geomarker.io/addr)" \
-  ##   -H "Sec-GPC: 1" \
-  ##   -o tl_2022_39061_addrfeat.zip
-
-  withr::local_envvar(list("R_USER_DATA_DIR" = tempdir()))
+  withr::local_envvar(list("R_USER_DATA_DIR" = tempfile()))
   dl_file <- tiger_download(
     "TIGER2024/INTERNATIONALBOUNDARY/tl_2024_us_internationalboundary.zip"
   )
-
-  sf::read_sf(paste0("/vsizip/", dl_file)) |>
-    ncol() |>
-    expect_identical(4L)
+  expect_true(file.exists(dl_file))
 })
 
-test_that("tiger_addr_feat() works", {
-  withr::local_envvar(list("R_USER_DATA_DIR" = tempdir()))
+test_that("tiger_addr_feat() can download addr feat from tiger", {
+  skip_if_offline()
+  skip_on_cran()
+  skip_on_ci()
+  withr::local_envvar(list("R_USER_DATA_DIR" = tempfile()))
+  d <- tiger_addr_feat(county = "39061", year = "2024")
+  expect_s3_class(d, c("sf", "tbl_df", "tbl", "data.frame"))
+  expect_true(nrow(d) > 0)
+  expect_true(all(c("LINEARID", "FULLNAME", "ZIP") %in% names(d)))
+  expect_s3_class(d$geometry, "sfc")
+})
+
+test_that("tiger_addr_feat() works with existing user data dir", {
+  skip_if_offline()
+  skip_on_cran()
+  skip_on_ci()
   d <- tiger_addr_feat(county = "39061", year = "2024")
   expect_s3_class(d, c("sf", "tbl_df", "tbl", "data.frame"))
   expect_true(nrow(d) > 0)
