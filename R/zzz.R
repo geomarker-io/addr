8e59c970e7a90de29480ff4d5d88a20f32f08b59
Cole Brokamp <cole.brokamp@gmail.com>
2026-02-25 23:23:21 -0500

    check_installed()

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,15 @@
 .onLoad <- function(...) {
   S7::methods_register()
 }
+
+check_installed <- function(pkg, reason = NULL) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    msg <- sprintf("The package `%s` is required", pkg)
+    if (!is.null(reason)) {
+      msg <- paste0(msg, " ", reason)
+    }
+    msg <- paste0(msg, ". Please install it first.")
+    stop(msg, call. = FALSE)
+  }
+  invisible(TRUE)
+}
