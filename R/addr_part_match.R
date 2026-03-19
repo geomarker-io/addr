f51a34bc58cba71027253ae8374b470f2121cd48
Cole Brokamp <cole.brokamp@gmail.com>
2026-03-19 17:00:34 -0400

    try new approach for just one street match

--- R/addr_part_match.R
+++ R/addr_part_match.R
@@ -1,8 +1,61 @@
-# matching functions all have the same behavior:
-# - return the matched entity (instead of its index value)
-# - all take the inputs x and y
-# - deduplicates computations on repeated components in x and y
-# - match returns the best single result, always returning a vector of matches instead of a list with multiple possible matches
+yy <- nad_example_data()$nad_addr@street
+
+match_one_street(
+  x = addr_street(
+    predirectional = "",
+    premodifier = "",
+    pretype = "",
+    name = "14th",
+    posttype = "st",
+    postdirectional = ""
+  ),
+  y = yy
+)
+
+match_one_street <- function(x, y, osa_max_dist = 2L) {
+  stopifnot(!is.na(x), length(x) == 1, x@name != "")
+  x_psk <- phonetic_street_key(x@name)
+  y_psk <- phonetic_street_key(y@name)
+  m <- unique(y[y_psk %in% x_psk])
+  # if no psk match within osa dist on name, then search for common phonetic
+  # variants that have names within osa dist
+  m_dist <- stringdist::stringdist(tolower(x@name), tolower(m@name))
+  if (length(m) == 0 || all(m_dist > osa_max_dist)) {
+    z <- strsplit(x_psk, "")[[1]]
+    x_psk_var <- c(
+      x_psk,
+      paste0(z[c(1, 3, 2, 4)], collapse = ""),
+      paste0(z[c(1, 2, 4, 3)], collapse = "")
+    )
+    m <- unique(y[y_psk %in% x_psk_var])
+    m_dist <- stringdist::stringdist(tolower(x@name), tolower(m@name))
+    m <- unique(m[m_dist <= osa_max_dist])
+  }
+  # if no exact nor fuzzy match on psk + street name, return NA
+  if (length(m) == 0) {
+    return(addr::addr_street())
+  }
+  # if present, narrow to matching posttype
+  m_type <- m[m@posttype == x@posttype]
+  if (length(m_type) > 0) {
+    m <- m_type
+  }
+  # if present, narrow to matching predir
+  m_predir <- m[m@predirectional == x@predirectional]
+  if (length(m_predir) > 0) {
+    m <- m_predir
+  }
+
+  browser()
+
+  m_dist <- stringdist::stringdist(
+    tolower(as.character(x)),
+    tolower(as.character(m)),
+    method = "osa"
+  )
+  m <- m[order(m_dist, sort(as.character(m)))]
+  m[1]
+}
 
 #' Match addr_street vectors
 #'
@@ -94,10 +147,10 @@ match_addr_street <- function(x, y) {
   out_l <- lkp[tolower(as.character(x))] |>
     lapply(\(.) uy[.])
   empties <- which(is.na(names(out_l)))
-  out_l[empties] <- replicate(length(empties), addr_street())
+  out_l[empties] <- replicate(length(empties), addr::addr_street())
   out <-
     do.call(rbind, lapply(out_l, as.data.frame)) |>
-    vec_restore(to = addr_street())
+    vec_restore(to = addr::addr_street())
   return(out)
 }
 
@@ -162,7 +215,7 @@ match_addr_number <- function(x, y, osa_max_dist = 1L) {
             osa_max_dist
         ]
         if (length(m) == 0) {
-          return(addr_number())
+          return(addr::addr_number())
         }
         m_sort <-
           m[order(
@@ -176,17 +229,17 @@ match_addr_number <- function(x, y, osa_max_dist = 1L) {
           )]
         return(m_sort[1])
       }
-      return(addr_number())
+      return(addr::addr_number())
     }
   )
   names(lkp) <- as.character(ux)
   lkp[sapply(lkp, is.na)] <- NULL # remove unmatched
   out_l <- lkp[as.character(x)]
   empties <- which(sapply(out_l, is.null))
-  out_l[empties] <- replicate(length(empties), addr_number())
+  out_l[empties] <- replicate(length(empties), addr::addr_number())
   out <-
     do.call(rbind, lapply(out_l, as.data.frame)) |>
-    vec_restore(to = addr_number())
+    vec_restore(to = addr::addr_number())
   return(out)
 }
 
@@ -217,8 +270,8 @@ match_addr_number <- function(x, y, osa_max_dist = 1L) {
 #'   zip_variants = FALSE
 #' )
 match_zipcodes <- function(x, y, zip_variants = TRUE) {
-  ux <- unique(addr_place(zipcode = x)@zipcode)
-  uy <- unique(addr_place(zipcode = y)@zipcode)
+  ux <- unique(addr::addr_place(zipcode = x)@zipcode)
+  uy <- unique(addr::addr_place(zipcode = y)@zipcode)
   lkp <- vapply(
     ux,
     FUN = \(xz) {
@@ -250,7 +303,7 @@ match_zipcodes <- function(x, y, zip_variants = TRUE) {
 
 zipcode_variant <- function(x) {
   stopifnot(typeof(x) == "character", length(x) == 1L)
-  x <- addr_place(zipcode = x)@zipcode
+  x <- addr::addr_place(zipcode = x)@zipcode
   if (is.na(x)) {
     return(NA_character_)
   }
