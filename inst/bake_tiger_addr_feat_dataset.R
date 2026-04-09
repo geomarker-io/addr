755512a0d4a01c06360e841332526a69fea093ea
Cole Brokamp <cole.brokamp@gmail.com>
2026-04-09 08:28:37 -0400

    bake tiger addr feat dataset

--- inst/bake_tiger_addr_feat_dataset.R
+++ inst/bake_tiger_addr_feat_dataset.R
@@ -3,6 +3,7 @@ devtools::load_all()
 mirai::daemons(parallelly::availableCores() - 1)
 
 cnty_fips <- county_fips_reference$county_fips
+cnty_fips <- cnty_fips[!substr(cnty_fips, 1, 2) %in% c("60", "69")]
 
 system.time({
   mm <- mirai::mirai_map(
@@ -12,5 +13,30 @@ system.time({
       taf_install(., year = "2025")
     }
   )
-  mmo <- mm[.flat, .stop, .progress]
+  mmo <- mm[.stop, .progress]
 })
+mirai::daemons(0)
+
+taf_fls <-
+  fs::dir_info(
+    fs::path(
+      tools::R_user_dir("addr", "data"),
+      "v1",
+      "tiger_addr_feat",
+      "2025"
+    ),
+    recurse = TRUE
+  ) |>
+  dplyr::filter(type == "file")
+
+message(
+  file.path(tools::R_user_dir("addr", "data"), "v1", "2025"),
+  " has ",
+  prettyNum(nrow(taf_fls), big.mark = ","),
+  " county files",
+  " totaling ",
+  sum(taf_fls$size)
+)
+
+# baking with pre-downloaded TIGER shapefiles takes
+# ~ 15min on my laptop using 9/10 cores
